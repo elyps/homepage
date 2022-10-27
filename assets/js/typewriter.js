f9c55a46a0a24b0b2574be60c10075d123bf75f5
Bastian Fischer <elyps@m1.lan>
2022-10-27 12:30:17 +0200

    first commit

---
+++ assets/js/typewriter.js
@@ -0,0 +1,16 @@
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(2500)
+  .typeString('<span style="color: rgba(232, 62, 140, 1.00);">Software</span> Developer, Designer ')
+  .pauseFor(300)
+  .deleteChars(9)
+  .typeString(' <span style="color: rgba(232, 62, 140, 1.00);">UI/UX</span> Designer ')
+  .typeString('and <span style="color: rgba(232, 62, 140, 1.00);">Open</span>-Sourcerer!')
+  .pauseFor(25000)
+  .start();
